9f70a2210d5d86427fd966e262939dc1e9746c96
= <=>
2020-03-30 14:08:33 +0800

    3000

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,6 +69,10 @@ module.exports = {
     // extend (config, ctx) {
     // }
   },
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0' // default: localhost,
+  },
   rules: {
     "vue/html-closing-bracket-newline": "error"
   }
